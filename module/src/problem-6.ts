8ca585af5ca4be12752793deb77923c8f7e2d776
Munshi Muntasir Rifat <rifat2002m@gmail.com>
2024-11-08 04:41:35 +0600

    problem-6

---
+++ module/src/problem-6.ts
@@ -0,0 +1,25 @@
+{
+    //
+    interface Profile{
+        name: string,
+        age: number,
+        email: string
+    }
+
+    function updateProfile(profile: Profile, updates: Partial<Profile>) : Profile {
+        return Object.assign({}, profile, updates);
+    };
+
+    const myProfile = {
+        name: "Alice", 
+        age: 25,
+        email: "alice@example.com" 
+    };
+    
+    //Result
+    console.log(updateProfile(myProfile, {age: 26 }));
+
+
+
+    //
+}
