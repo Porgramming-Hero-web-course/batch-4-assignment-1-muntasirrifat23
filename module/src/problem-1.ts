9384540427912915b32db96ab200d031cdbaf788
Munshi Muntasir Rifat <rifat2002m@gmail.com>
2024-11-07 21:11:25 +0600

    tsc add

---
+++ module/src/problem-1.ts
@@ -0,0 +1,15 @@
+{
+    //
+    function addAll(number1: number, number2:number, number3: number, number4:number, number5: number){
+        return number1+number2+number3+number4+number5;
+    }
+    addAll(1,2,3,4,5);
+    console.log(addAll)
+
+
+
+    
+    
+    //
+
+}
