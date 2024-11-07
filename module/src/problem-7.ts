7eb501f3e1d6744cc83d5faf19f6f96611b54ed5
Munshi Muntasir Rifat <rifat2002m@gmail.com>
2024-11-08 04:59:15 +0600

    problem-7

--- module/src/problem-7.ts
+++ module/src/problem-7.ts
@@ -1,5 +1,17 @@
 {
     //
+    class Car{
+        constructor(public make: string, public model: string, public year: number){}
+
+        getCarAge(): number{
+            const currentYear = new Date().getFullYear();
+            return currentYear - this.year;
+        }
+    }
+
+    const car = new Car("Honda", "Civic", 2018);
+    //Result
+    console.log(`Car age is:`, car.getCarAge())
 
     //
 }
