7ec75308de38629db2388cd53a0793d19f9b2b7a
Munshi Muntasir Rifat <rifat2002m@gmail.com>
2024-11-07 22:05:48 +0600

    problem-2

---
+++ module/src/problem-2.ts
@@ -0,0 +1,18 @@
+{
+    //
+    
+   function removeDuplicates(Values: number[]) : number[] {
+    let output: number[] =[];
+    for(let value of Values){
+        if(!output.includes(value)){
+            output.push(value);
+        }
+    }
+    return output;
+   }
+
+   console.log(removeDuplicates([1,2,2,3,4,4,5]))
+
+
+    //
+}
